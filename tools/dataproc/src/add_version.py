dc8302124938734a9ed2958e456e5d16896a5e8d
MisterWhopper <levi.shaffer@misterwhopper.org>
2026-06-05 23:00:43 -0500

    Version: Add indicator if there are unsynced changes to git repo
    Now 'git-<hash>' becomes '*git-<hash>'

--- tools/dataproc/src/add_version.py
+++ tools/dataproc/src/add_version.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from dataclasses import dataclass
 from pathlib import Path
 from string import Template
 import subprocess
@@ -11,22 +12,35 @@ TEMPLATE_FILE = BASE_PATH.parent / "data" / "title_screen.json.template"
 VERSION_FILE = PROJ_PATH / ".version.txt"
 
 
-def try_get_git_short_hash() -> str:
+@dataclass
+class GitData:
+    last_commit: str
+    is_synced: bool
+
+
+def try_get_git_short_hash() -> GitData:
     try:
-        proc = subprocess.run(
+        sha_proc = subprocess.run(
             ["git", "rev-parse", "--short", "HEAD"], check=True, capture_output=True
+        )
+        status_proc = subprocess.run(
+            ["git", "status", "--short"], check=True, capture_output=True
         )
     except Exception as err:
         print(f"ERROR: {err}")
         exit(1)
     else:
-        return proc.stdout.decode().strip()
+        return GitData(
+            last_commit=sha_proc.stdout.decode().strip(),
+            is_synced=len(status_proc.stdout.decode().strip()) == 0,
+        )
 
 
 def try_get_version() -> str:
     if VERSION_FILE.exists() and VERSION_FILE.is_file():
         return f"v{VERSION_FILE.read_text()}"
-    return f"git-{try_get_git_short_hash()}"
+    git_data = try_get_git_short_hash()
+    return f"{'*' if not git_data.is_synced else ''}git-{git_data.last_commit}"
 
 
 def main():
